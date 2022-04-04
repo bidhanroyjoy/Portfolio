eab0014f56e65e5cb9059b8c85ce3c29693b91de
bidhanroyjoy <bidhan19994@gmail.com>
2022-04-04 20:35:20 +0600

    file structure build

---
+++ src/components/Toogle/Toogle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Toogle.css";
+
+const Toogle = () => {
+  return (
+    <div>Toogle</div>
+  )
+}
+
+export default Toogle
---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import Card from "./Card/Card";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+import Experience from "./Experience/Experience";
+import FloatingDiv from "./FloatingDiv/FloatingDiv";
+import Intro from "./Intro/Intro";
+import Navbar from "./Navbar/Navbar";
+import Portfolio from "./Portfolio/Portfolio";
+import Services from "./Services/Services";
+import Testimonial from "./Testimonials/Testimonial";
+import Toogle from "./Toogle/Toogle";
+import Works from "./Works/Works";
+
+
+export{
+    Card,
+    Contact,
+    Footer,
+    Experience,
+    FloatingDiv,
+    Intro,
+    Navbar,
+    Portfolio,
+    Services,
+    Testimonial,
+    Toogle,
+    Works,
+}
